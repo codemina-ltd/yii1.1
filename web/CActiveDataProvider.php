6386f20418f3eb9fd903f454e7d5df22c8f2dc14
Ebrahim Alhaj <codemina@outlook.com>
2022-09-15 05:38:34 +0200

    [master] Update pagination to set item count as int

--- web/CActiveDataProvider.php
+++ web/CActiveDataProvider.php
@@ -46,187 +46,179 @@
  */
 class CActiveDataProvider extends CDataProvider
 {
-	/**
-	 * @var string the primary ActiveRecord class name. The {@link getData()} method
-	 * will return a list of objects of this class.
-	 */
-	public $modelClass;
-	/**
-	 * @var CActiveRecord the AR finder instance (eg <code>Post::model()</code>).
-	 * This property can be set by passing the finder instance as the first parameter
-	 * to the constructor. For example, <code>Post::model()->published()</code>.
-	 * @since 1.1.3
-	 */
-	public $model;
-	/**
-	 * @var string the name of key attribute for {@link modelClass}. If not set,
-	 * it means the primary key of the corresponding database table will be used.
-	 */
-	public $keyAttribute;
+    /**
+     * @var string the primary ActiveRecord class name. The {@link getData()} method
+     * will return a list of objects of this class.
+     */
+    public $modelClass;
+    /**
+     * @var CActiveRecord the AR finder instance (eg <code>Post::model()</code>).
+     * This property can be set by passing the finder instance as the first parameter
+     * to the constructor. For example, <code>Post::model()->published()</code>.
+     * @since 1.1.3
+     */
+    public $model;
+    /**
+     * @var string the name of key attribute for {@link modelClass}. If not set,
+     * it means the primary key of the corresponding database table will be used.
+     */
+    public $keyAttribute;
 
-	/**
-	 * @var CDbCriteria
-	 */
-	private $_criteria;
-	/**
-	 * @var CDbCriteria
-	 */
-	private $_countCriteria;
+    /**
+     * @var CDbCriteria
+     */
+    private $_criteria;
+    /**
+     * @var CDbCriteria
+     */
+    private $_countCriteria;
 
-	/**
-	 * Constructor.
-	 * @param mixed $modelClass the model class (e.g. 'Post') or the model finder instance
-	 * (e.g. <code>Post::model()</code>, <code>Post::model()->published()</code>).
-	 * @param array $config configuration (name=>value) to be applied as the initial property values of this class.
-	 */
-	public function __construct($modelClass,$config=array())
-	{
-		if(is_string($modelClass))
-		{
-			$this->modelClass=$modelClass;
-			$this->model=$this->getModel($this->modelClass);
-		}
-		elseif($modelClass instanceof CActiveRecord)
-		{
-			$this->modelClass=get_class($modelClass);
-			$this->model=$modelClass;
-		}
-		$this->setId(CHtml::modelName($this->model));
-		foreach($config as $key=>$value)
-			$this->$key=$value;
-	}
+    /**
+     * Constructor.
+     * @param mixed $modelClass the model class (e.g. 'Post') or the model finder instance
+     * (e.g. <code>Post::model()</code>, <code>Post::model()->published()</code>).
+     * @param array $config configuration (name=>value) to be applied as the initial property values of this class.
+     */
+    public function __construct($modelClass, $config = array())
+    {
+        if (is_string($modelClass)) {
+            $this->modelClass = $modelClass;
+            $this->model = $this->getModel($this->modelClass);
+        } elseif ($modelClass instanceof CActiveRecord) {
+            $this->modelClass = get_class($modelClass);
+            $this->model = $modelClass;
+        }
+        $this->setId(CHtml::modelName($this->model));
+        foreach ($config as $key => $value)
+            $this->$key = $value;
+    }
 
-	/**
-	 * Returns the query criteria.
-	 * @return CDbCriteria the query criteria
-	 */
-	public function getCriteria()
-	{
-		if($this->_criteria===null)
-			$this->_criteria=new CDbCriteria;
-		return $this->_criteria;
-	}
+    /**
+     * Given active record class name returns new model instance.
+     *
+     * @param string $className active record class name.
+     * @return CActiveRecord active record model instance.
+     *
+     * @since 1.1.14
+     */
+    protected function getModel($className)
+    {
+        return CActiveRecord::model($className);
+    }
 
-	/**
-	 * Sets the query criteria.
-	 * @param CDbCriteria|array $value the query criteria. This can be either a CDbCriteria object or an array
-	 * representing the query criteria.
-	 */
-	public function setCriteria($value)
-	{
-		$this->_criteria=$value instanceof CDbCriteria ? $value : new CDbCriteria($value);
-	}
+    /**
+     * Fetches the data from the persistent data storage.
+     * @return array list of data items
+     */
+    protected function fetchData()
+    {
+        $criteria = clone $this->getCriteria();
 
-	/**
-	 * Returns the count query criteria.
-	 * @return CDbCriteria the count query criteria.
-	 * @since 1.1.14
-	 */
-	public function getCountCriteria()
-	{
-		if($this->_countCriteria===null)
-			return $this->getCriteria();
-		return $this->_countCriteria;
-	}
+        if (($pagination = $this->getPagination()) !== false) {
+            $pagination->setItemCount($this->getTotalItemCount());
+            $pagination->applyLimit($criteria);
+        }
 
-	/**
-	 * Sets the count query criteria.
-	 * @param CDbCriteria|array $value the count query criteria. This can be either a CDbCriteria object
-	 * or an array representing the query criteria.
-	 * @since 1.1.14
-	 */
-	public function setCountCriteria($value)
-	{
-		$this->_countCriteria=$value instanceof CDbCriteria ? $value : new CDbCriteria($value);
-	}
+        $baseCriteria = $this->model->getDbCriteria(false);
 
-	/**
-	 * Returns the sorting object.
-	 * @param string $className the sorting object class name. Parameter is available since version 1.1.13.
-	 * @return CSort the sorting object. If this is false, it means the sorting is disabled.
-	 */
-	public function getSort($className='CSort')
-	{
-		if(($sort=parent::getSort($className))!==false)
-			$sort->modelClass=$this->modelClass;
-		return $sort;
-	}
+        if (($sort = $this->getSort()) !== false) {
+            // set model criteria so that CSort can use its table alias setting
+            if ($baseCriteria !== null) {
+                $c = clone $baseCriteria;
+                $c->mergeWith($criteria);
+                $this->model->setDbCriteria($c);
+            } else
+                $this->model->setDbCriteria($criteria);
+            $sort->applyOrder($criteria);
+        }
 
-	/**
-	 * Given active record class name returns new model instance.
-	 *
-	 * @param string $className active record class name.
-	 * @return CActiveRecord active record model instance.
-	 *
-	 * @since 1.1.14
-	 */
-	protected function getModel($className)
-	{
-		return CActiveRecord::model($className);
-	}
+        $this->model->setDbCriteria($baseCriteria !== null ? clone $baseCriteria : null);
+        $data = $this->model->findAll($criteria);
+        $this->model->setDbCriteria($baseCriteria);  // restore original criteria
+        return $data;
+    }
 
-	/**
-	 * Fetches the data from the persistent data storage.
-	 * @return array list of data items
-	 */
-	protected function fetchData()
-	{
-		$criteria=clone $this->getCriteria();
+    /**
+     * Returns the query criteria.
+     * @return CDbCriteria the query criteria
+     */
+    public function getCriteria()
+    {
+        if ($this->_criteria === null)
+            $this->_criteria = new CDbCriteria;
+        return $this->_criteria;
+    }
 
-		if(($pagination=$this->getPagination())!==false)
-		{
-			$pagination->setItemCount($this->getTotalItemCount());
-			$pagination->applyLimit($criteria);
-		}
+    /**
+     * Sets the query criteria.
+     * @param CDbCriteria|array $value the query criteria. This can be either a CDbCriteria object or an array
+     * representing the query criteria.
+     */
+    public function setCriteria($value)
+    {
+        $this->_criteria = $value instanceof CDbCriteria ? $value : new CDbCriteria($value);
+    }
 
-		$baseCriteria=$this->model->getDbCriteria(false);
+    /**
+     * Returns the sorting object.
+     * @param string $className the sorting object class name. Parameter is available since version 1.1.13.
+     * @return CSort the sorting object. If this is false, it means the sorting is disabled.
+     */
+    public function getSort($className = 'CSort')
+    {
+        if (($sort = parent::getSort($className)) !== false)
+            $sort->modelClass = $this->modelClass;
+        return $sort;
+    }
 
-		if(($sort=$this->getSort())!==false)
-		{
-			// set model criteria so that CSort can use its table alias setting
-			if($baseCriteria!==null)
-			{
-				$c=clone $baseCriteria;
-				$c->mergeWith($criteria);
-				$this->model->setDbCriteria($c);
-			}
-			else
-				$this->model->setDbCriteria($criteria);
-			$sort->applyOrder($criteria);
-		}
+    /**
+     * Fetches the data item keys from the persistent data storage.
+     * @return array list of data item keys.
+     */
+    protected function fetchKeys()
+    {
+        $keys = array();
+        foreach ($this->getData() as $i => $data) {
+            $key = $this->keyAttribute === null ? $data->getPrimaryKey() : $data->{$this->keyAttribute};
+            $keys[$i] = is_array($key) ? implode(',', $key) : $key;
+        }
+        return $keys;
+    }
 
-		$this->model->setDbCriteria($baseCriteria!==null ? clone $baseCriteria : null);
-		$data=$this->model->findAll($criteria);
-		$this->model->setDbCriteria($baseCriteria);  // restore original criteria
-		return $data;
-	}
+    /**
+     * Calculates the total number of data items.
+     * @return integer the total number of data items.
+     */
+    protected function calculateTotalItemCount()
+    {
+        $baseCriteria = $this->model->getDbCriteria(false);
+        if ($baseCriteria !== null)
+            $baseCriteria = clone $baseCriteria;
+        $count = $this->model->count($this->getCountCriteria());
+        $this->model->setDbCriteria($baseCriteria);
+        return $count;
+    }
 
-	/**
-	 * Fetches the data item keys from the persistent data storage.
-	 * @return array list of data item keys.
-	 */
-	protected function fetchKeys()
-	{
-		$keys=array();
-		foreach($this->getData() as $i=>$data)
-		{
-			$key=$this->keyAttribute===null ? $data->getPrimaryKey() : $data->{$this->keyAttribute};
-			$keys[$i]=is_array($key) ? implode(',',$key) : $key;
-		}
-		return $keys;
-	}
+    /**
+     * Returns the count query criteria.
+     * @return CDbCriteria the count query criteria.
+     * @since 1.1.14
+     */
+    public function getCountCriteria()
+    {
+        if ($this->_countCriteria === null)
+            return $this->getCriteria();
+        return $this->_countCriteria;
+    }
 
-	/**
-	 * Calculates the total number of data items.
-	 * @return integer the total number of data items.
-	 */
-	protected function calculateTotalItemCount()
-	{
-		$baseCriteria=$this->model->getDbCriteria(false);
-		if($baseCriteria!==null)
-			$baseCriteria=clone $baseCriteria;
-		$count=$this->model->count($this->getCountCriteria());
-		$this->model->setDbCriteria($baseCriteria);
-		return $count;
-	}
+    /**
+     * Sets the count query criteria.
+     * @param CDbCriteria|array $value the count query criteria. This can be either a CDbCriteria object
+     * or an array representing the query criteria.
+     * @since 1.1.14
+     */
+    public function setCountCriteria($value)
+    {
+        $this->_countCriteria = $value instanceof CDbCriteria ? $value : new CDbCriteria($value);
+    }
 }
